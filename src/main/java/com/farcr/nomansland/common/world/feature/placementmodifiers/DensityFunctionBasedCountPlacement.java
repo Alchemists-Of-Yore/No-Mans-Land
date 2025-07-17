ca93b2a5bb6896aae1b1c4f2ff7427776aacc9d7
birsy <evan.c.muncy@gmail.com>
2025-07-16 21:30:04 -0400

    remap functionality for the density function based placements
    a little more difficult to understand, but more powerful.

--- src/main/java/com/farcr/nomansland/common/world/feature/placementmodifiers/DensityFunctionBasedCountPlacement.java
+++ src/main/java/com/farcr/nomansland/common/world/feature/placementmodifiers/DensityFunctionBasedCountPlacement.java
@@ -7,6 +7,7 @@ import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
+import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.levelgen.DensityFunction;
 import net.minecraft.world.level.levelgen.DensityFunctions;
@@ -18,47 +19,71 @@ import net.minecraft.world.level.levelgen.synth.NormalNoise;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-// "density_function_based_count" placement type!
-//
-// variation of the noise_based_count placement type,
-// taking in a density function rather than hardcoded
-// noise values.
-// this allows for more complicated curves, etc.
-//
-// EXAMPLE SYNTAX:
-//   {
-//      "type": "nomansland:density_function_based_count",
-//      "noise_function": "minecraft:overworld/base_3d_noise", // defaults to some reasonable noise.
-//                                                                can be a path to any density function, or inlined.
-//      "noise_to_count_ratio": 5, // required - no default.
-//                                    describes the density of feature placements.
-//      "noise_factor": 1.0, // defaults to 1.0.
-//                              a multiplier for the frequency of noise.
-//      "noise_offset": -0.25 // defaults to 0.0.
-//                               adds a value to the noise!
-//   }
+/*
+     "density_function_based_count" placement type!
+
+     variation of the noise_based_count placement type,
+     taking in a density function rather than hardcoded
+     noise values. this allows for more complicated curves, etc.
+     the placement position is duplicated based off the noise value.
+
+     EXAMPLE SYNTAX:
+       {
+          "type": "nomansland:density_function_based_count",
+          "noise_function": "minecraft:overworld/base_3d_noise", // noise router. defaults to some reasonable noise.
+                                                                    can be a path to any density function, or inlined.
+          "noise_scale": 1.0,         // decimal number. defaults to 1.0.
+                                         a multiplier for the frequency of noise.
+          // the following parameters are for remapping noise range.
+          // useful for rescaling vanilla functions to fit a desired appearance!
+          // the default values make it so no rescaling takes place.
+          //     IF YOU DON'T QUITE GET RESCALING - look up the 'Map Range' function.
+          //         a good reference: https://processing.org/reference/map_.html
+          "noise_minimum": 0.0, // decimal number. defaults to 0.0.
+                                   the noise value that will be mapped to the minimum count
+          "noise_maximum": 1.0, // decimal number. defaults to 1.0.
+                                   the noise value that will be mapped to the maximum count
+          "count_minimum": 0, // integer. defaults to 0.
+                                         the value which noise_minimum is mapped to.
+                                         is not necessarily the minimum possible count, if clamped is false.
+          "count_maximum": 12, // integer. defaults to 8.
+                                         the value which noise_maximum is mapped to.
+                                         is not necessarily the maximum possible count, if clamped is false.
+          "clamped": true // boolean. defaults to true.
+                             determines whether to clamp the probability to the given range.
+       }
+*/
 public class DensityFunctionBasedCountPlacement extends PlacementModifier {
     private static final DensityFunction DEFAULT_DENSITY_FUNCTION =
             DensityFunctions.noise(Holder.direct(new NormalNoise.NoiseParameters(-4, 1)));
     public static final MapCodec<DensityFunctionBasedCountPlacement> CODEC = RecordCodecBuilder.mapCodec (
             codec -> codec.group(
                             DensityFunction.HOLDER_HELPER_CODEC.fieldOf("noise_function").orElse(DEFAULT_DENSITY_FUNCTION).forGetter(instance -> instance.densityFunction),
-                            Codec.INT.fieldOf("noise_to_count_ratio").forGetter(instance -> instance.noiseToCountRatio),
                             Codec.DOUBLE.fieldOf("noise_scale").orElse(1.0).forGetter(instance -> instance.noiseScale),
-                            Codec.DOUBLE.fieldOf("noise_offset").orElse(0.0).forGetter(instance -> instance.noiseOffset)
+                            Codec.DOUBLE.fieldOf("noise_minimum").orElse(0.0).forGetter(instance -> instance.noiseMinimum),
+                            Codec.DOUBLE.fieldOf("noise_maximum").orElse(1.0).forGetter(instance -> instance.noiseMaximum),
+                            Codec.INT.fieldOf("count_minimum").orElse(0).forGetter(instance -> instance.countMinimum),
+                            Codec.INT.fieldOf("count_maximum").orElse(8).forGetter(instance -> instance.countMaximum),
+                            Codec.BOOL.fieldOf("clamped").orElse(true).forGetter(instance -> instance.clamped)
                     ).apply(codec, DensityFunctionBasedCountPlacement::new)
     );
 
     private final DensityFunction densityFunction;
-    private final int noiseToCountRatio;
     private final double noiseScale;
-    private final double noiseOffset;
+    private final double noiseMinimum;
+    private final double noiseMaximum;
+    private final int countMinimum;
+    private final int countMaximum;
+    private final boolean clamped;
 
-    public DensityFunctionBasedCountPlacement(DensityFunction densityFunction, int noiseToCountRatio, double noiseScale, double noiseOffset) {
+    public DensityFunctionBasedCountPlacement(DensityFunction densityFunction, double noiseScale, double noiseMinimum, double noiseMaximum, int countMinimum, int countMaximum, boolean clamped) {
         this.densityFunction = densityFunction;
-        this.noiseToCountRatio = noiseToCountRatio;
         this.noiseScale = noiseScale;
-        this.noiseOffset = noiseOffset;
+        this.noiseMinimum = noiseMinimum;
+        this.noiseMaximum = noiseMaximum;
+        this.countMinimum = countMinimum;
+        this.countMaximum = countMaximum;
+        this.clamped = clamped;
     }
 
     @Override
@@ -77,7 +102,9 @@ public class DensityFunctionBasedCountPlacement extends PlacementModifier {
         // uncomment this line to preview noise values!
         // if (random.nextInt(16) == 0) NoMansLand.LOGGER.info(noise);
 
-        int count = (int) Math.ceil((noise + this.noiseOffset) * this.noiseToCountRatio);
+        int count = (int) Math.ceil(clamped ?
+                Mth.clampedMap(noise, noiseMinimum, noiseMaximum, countMinimum, countMaximum) :
+                Mth.map(noise, noiseMinimum, noiseMaximum, countMinimum, countMaximum));
         return IntStream.range(0, count)
                 .mapToObj(i -> pos);
     }
--- src/main/java/com/farcr/nomansland/common/world/feature/placementmodifiers/DensityFunctionBasedProbabilityPlacement.java
+++ src/main/java/com/farcr/nomansland/common/world/feature/placementmodifiers/DensityFunctionBasedProbabilityPlacement.java
@@ -1,6 +1,5 @@
 package com.farcr.nomansland.common.world.feature.placementmodifiers;
 
-import com.farcr.nomansland.NoMansLand;
 import com.farcr.nomansland.common.registry.worldgen.NMLPlacementModifiers;
 import com.farcr.nomansland.common.world.generation.LazilyCachedDensityFunctionSeedifier;
 import com.mojang.serialization.Codec;
@@ -8,7 +7,6 @@ import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
-import net.minecraft.server.level.ServerChunkCache;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.levelgen.DensityFunction;
@@ -20,26 +18,41 @@ import net.minecraft.world.level.levelgen.synth.NormalNoise;
 
 import java.util.stream.Stream;
 
-// "density_function_based_probability" placement type!
-//
-// variation of the density_function_based_count placement type,
-// removing placement spots based off a noise value rather than
-// adding new ones.
-//
-// EXAMPLE SYNTAX:
-//   {
-//      "type": "nomansland:density_function_based_probability",
-//      "noise_function": "minecraft:overworld/base_3d_noise", // defaults to some reasonable noise.
-//                                                                can be a path to any density function, or inlined.
-//      "noise_scale": 1.0,         // defaults to 1.0.
-//                                     a multiplier for the frequency of noise.
-//      "minimum_probability": 0.0, // defaults to 0.0.
-//                                     the minimum probability that a given position will be kept.
-//      "maximum_probability": 1.0, // defaults to 1.0.
-//                                     the maximum probability that a given position will be kept.
-//      "normalize_noise": true // defaults to false. determines whether to rescale the noise to always fall between the minimum and maximum
-//                                 probability, or simply to clamp it.
-//   }
+/*
+     "density_function_based_probability" placement type!
+
+     variation of the density_function_based_count placement type,
+     removing placement spots based off a noise value rather than
+     adding new ones.
+     the sampled noise value is remapped to the probability range,
+     then used as a % chance that a given position is KEPT.
+
+     EXAMPLE SYNTAX:
+       {
+          "type": "nomansland:density_function_based_probability",
+          "noise_function": "minecraft:overworld/base_3d_noise", // noise router. defaults to some reasonable noise.
+                                                                    can be a path to any density function, or inlined.
+          "noise_scale": 1.0,         // decimal number. defaults to 1.0.
+                                         a multiplier for the frequency of noise.
+          // the following parameters are for remapping noise range.
+          // useful for rescaling vanilla functions to fit a desired appearance!
+          // the default values make it so no rescaling takes place.
+          //     IF YOU DON'T QUITE GET RESCALING - look up the 'Map Range' function.
+          //         a good reference: https://processing.org/reference/map_.html
+          "noise_minimum": 0.0, // decimal number. defaults to 0.0.
+                                   the noise value that will be mapped to the minimum probability
+          "noise_maximum": 1.0, // decimal number. defaults to 1.0.
+                                   the noise value that will be mapped to the maximum probability
+          "probability_minimum": 0.0, // decimal number. defaults to 0.0.
+                                         the value which noise_minimum is mapped to.
+                                         is not necessarily the minimum possible probability, if clamped is false.
+          "probability_maximum": 1.0, // decimal number. defaults to 1.0.
+                                         the value which noise_maximum is mapped to.
+                                         is not necessarily the maximum possible probability, if clamped is false.
+          "clamped": true // boolean. defaults to true.
+                             determines whether to clamp the probability to the given range.
+       }
+*/
 public class DensityFunctionBasedProbabilityPlacement extends PlacementModifier {
     private static final DensityFunction DEFAULT_DENSITY_FUNCTION =
             DensityFunctions.noise(Holder.direct(new NormalNoise.NoiseParameters(-4, 1)));
@@ -48,24 +61,30 @@ public class DensityFunctionBasedProbabilityPlacement extends PlacementModifier 
             codec -> codec.group(
                             DensityFunction.HOLDER_HELPER_CODEC.fieldOf("noise_function").orElse(DEFAULT_DENSITY_FUNCTION).forGetter(instance -> instance.densityFunction),
                             Codec.DOUBLE.fieldOf("noise_scale").orElse(1.0).forGetter(instance -> instance.noiseScale),
-                            Codec.DOUBLE.fieldOf("minimum_probability").orElse(0.0).forGetter(instance -> instance.minimumProbability),
-                            Codec.DOUBLE.fieldOf("maximum_probability").orElse(1.0).forGetter(instance -> instance.maximumProbability),
-                            Codec.BOOL.fieldOf("normalize_noise").orElse(false).forGetter(instance -> instance.normalizeNoise)
-                    ).apply(codec, DensityFunctionBasedProbabilityPlacement::new)
+                            Codec.DOUBLE.fieldOf("noise_minimum").orElse(0.0).forGetter(instance -> instance.noiseMinimum),
+                            Codec.DOUBLE.fieldOf("noise_maximum").orElse(1.0).forGetter(instance -> instance.noiseMaximum),
+                            Codec.DOUBLE.fieldOf("probability_minimum").orElse(0.0).forGetter(instance -> instance.probabilityMinimum),
+                            Codec.DOUBLE.fieldOf("probability_maximum").orElse(1.0).forGetter(instance -> instance.probabilityMaximum),
+                            Codec.BOOL.fieldOf("clamped").orElse(true).forGetter(instance -> instance.clamped)
+            ).apply(codec, DensityFunctionBasedProbabilityPlacement::new)
     );
 
     private final DensityFunction densityFunction;
     private final double noiseScale;
-    private final double minimumProbability;
-    private final double maximumProbability;
-    private final boolean normalizeNoise;
+    private final double noiseMinimum;
+    private final double noiseMaximum;
+    private final double probabilityMinimum;
+    private final double probabilityMaximum;
+    private final boolean clamped;
 
-    public DensityFunctionBasedProbabilityPlacement(DensityFunction densityFunction, double noiseScale, double minimumProbability, double maximumProbability, boolean normalizeNoise) {
+    public DensityFunctionBasedProbabilityPlacement(DensityFunction densityFunction, double noiseScale, double noiseMinimum, double noiseMaximum, double probabilityMinimum, double probabilityMaximum, boolean clamped) {
         this.densityFunction = densityFunction;
         this.noiseScale = noiseScale;
-        this.minimumProbability = minimumProbability;
-        this.maximumProbability = maximumProbability;
-        this.normalizeNoise = normalizeNoise;
+        this.noiseMinimum = noiseMinimum;
+        this.noiseMaximum = noiseMaximum;
+        this.probabilityMinimum = probabilityMinimum;
+        this.probabilityMaximum = probabilityMaximum;
+        this.clamped = clamped;
     }
 
     @Override
@@ -85,12 +104,9 @@ public class DensityFunctionBasedProbabilityPlacement extends PlacementModifier 
         // uncomment this line to preview noise values!
         // if (random.nextInt(32) == 0) NoMansLand.LOGGER.info(threshold);
 
-        if (normalizeNoise) {
-            threshold = Mth.clampedMap(threshold, densityFunction.minValue(), densityFunction.maxValue(),  minimumProbability, maximumProbability);
-        } else {
-            threshold = Mth.clamp(threshold, minimumProbability, maximumProbability);
-        }
-
+        threshold = clamped ?
+                Mth.clampedMap(threshold, noiseMinimum, noiseMaximum, probabilityMinimum, probabilityMaximum) :
+                       Mth.map(threshold, noiseMinimum, noiseMaximum, probabilityMinimum, probabilityMaximum);
         return random.nextDouble() < threshold ? Stream.of(pos) : Stream.empty();
     }
 
