c6874c7d321ed6a94d29db2be91a5a9e33f8f1a1
birsy <evan.c.muncy@gmail.com>
2025-07-30 22:36:45 -0400

    height filter filters height

---
+++ src/main/java/com/farcr/nomansland/common/world/feature/placementmodifiers/HeightFilterPlacement.java
@@ -0,0 +1,58 @@
+package com.farcr.nomansland.common.world.feature.placementmodifiers;
+
+import com.farcr.nomansland.common.registry.worldgen.NMLPlacementModifiers;
+import com.farcr.nomansland.common.world.densityfunction.LazilyCachedDensityFunctionSeedifier;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.BlockPos;
+import net.minecraft.util.Mth;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.levelgen.DensityFunction;
+import net.minecraft.world.level.levelgen.placement.PlacementContext;
+import net.minecraft.world.level.levelgen.placement.PlacementFilter;
+import net.minecraft.world.level.levelgen.placement.PlacementModifier;
+import net.minecraft.world.level.levelgen.placement.PlacementModifierType;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/*
+     "height_filter" placement type!
+
+     removes placements that don't fall between a minimum and maximum height.
+
+     EXAMPLE SYNTAX:
+       {
+          "type": "nomansland:height_filter",
+          "height_min": 0,  // integer. defaults to -1000000. (no minimum)
+                                        inclusive. the minimum height that a position isn't filtered.
+          "height_max": 12, // integer. defaults to 1000000. (no maximum)
+                                        inclusive. the maximum height that a position isn't filtered.
+       }
+*/
+public class HeightFilterPlacement extends PlacementFilter {
+    public static final MapCodec<HeightFilterPlacement> CODEC = RecordCodecBuilder.mapCodec (
+            codec -> codec.group(
+                            Codec.INT.fieldOf("height_min").orElse(-1000000).forGetter(instance -> instance.minimumHeight),
+                            Codec.INT.fieldOf("height_max").orElse( 1000000).forGetter(instance -> instance.maximumHeight)
+                    ).apply(codec, HeightFilterPlacement::new)
+    );
+
+    private final int minimumHeight, maximumHeight;
+
+    public HeightFilterPlacement(int minimumHeight, int maximumHeight) {
+        this.minimumHeight = minimumHeight;
+        this.maximumHeight = maximumHeight;
+    }
+
+    @Override
+    protected boolean shouldPlace(PlacementContext context, RandomSource random, BlockPos pos) {
+        return pos.getY() >= minimumHeight && pos.getY() <= maximumHeight;
+    }
+
+    @Override
+    public PlacementModifierType<?> type() {
+        return NMLPlacementModifiers.HEIGHT_FILTER.get();
+    }
+}
