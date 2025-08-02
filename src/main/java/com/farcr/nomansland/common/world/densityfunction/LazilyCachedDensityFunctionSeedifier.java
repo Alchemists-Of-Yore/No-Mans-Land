6c2c6407ed0a98afc5fc97a20a8aaa127453593a
birsy <evan.c.muncy@gmail.com>
2025-08-02 11:24:32 -0400

    make this less painfully slow...

--- src/main/java/com/farcr/nomansland/common/world/densityfunction/LazilyCachedDensityFunctionSeedifier.java
+++ src/main/java/com/farcr/nomansland/common/world/densityfunction/LazilyCachedDensityFunctionSeedifier.java
@@ -6,6 +6,7 @@ import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.dimension.DimensionType;
 import net.minecraft.world.level.levelgen.DensityFunction;
+import net.minecraft.world.level.levelgen.NoiseSettings;
 import net.minecraft.world.level.levelgen.RandomState;
 import net.minecraft.world.level.levelgen.synth.NormalNoise;
 
@@ -18,10 +19,11 @@ import java.util.concurrent.ConcurrentHashMap;
 // trying to do this without making a billion extra new objects
 // and overwhelming the garbage collector.
 public abstract class LazilyCachedDensityFunctionSeedifier implements DensityFunction.Visitor {
-    private static final Map<WorldGeneratorEntry, LazilyCachedDensityFunctionSeedifier> visitorCache = new ConcurrentHashMap<>();
+    private static final ThreadLocal<Map<WorldGeneratorEntry, LazilyCachedDensityFunctionSeedifier>> visitorCache =
+            ThreadLocal.withInitial(HashMap::new);
 
     public static DensityFunction.Visitor getOrCreate(WorldGenLevel worldGenLevel) {
-        return visitorCache.computeIfAbsent(new WorldGeneratorEntry(worldGenLevel.getSeed(), worldGenLevel.dimensionType()), (level) -> {
+        return visitorCache.get().computeIfAbsent(new WorldGeneratorEntry(worldGenLevel.getSeed(), worldGenLevel.dimensionType()), (level) -> {
             if (worldGenLevel.getChunkSource() instanceof ServerChunkCache chunkCache) {
                 return new RandomStateBasedDensityFunctionSeedifier(chunkCache.randomState());
             } else {
@@ -30,13 +32,13 @@ public abstract class LazilyCachedDensityFunctionSeedifier implements DensityFun
         });
     }
     public static void clearCache() {
-        visitorCache.clear();
+        visitorCache.get().clear();
     }
 
-    private final Map<ResourceKey, DensityFunction.NoiseHolder> holderCache;
+    private final ThreadLocal<Map<ResourceKey<NormalNoise.NoiseParameters>, DensityFunction.NoiseHolder>> holderCache;
 
     public LazilyCachedDensityFunctionSeedifier() {
-        this.holderCache = new ConcurrentHashMap<>();
+        this.holderCache =  ThreadLocal.withInitial(HashMap::new);
     }
 
     @Override
@@ -47,7 +49,7 @@ public abstract class LazilyCachedDensityFunctionSeedifier implements DensityFun
     @Override
     public DensityFunction.NoiseHolder visitNoise(DensityFunction.NoiseHolder noiseHolder) {
         NormalNoise.NoiseParameters noiseParameters = noiseHolder.noiseData().value();
-        return this.holderCache.computeIfAbsent(noiseHolder.noiseData().unwrapKey().orElseThrow(), (resourceKey) -> modifyNoiseHolder(noiseHolder, noiseParameters));
+        return this.holderCache.get().computeIfAbsent(noiseHolder.noiseData().unwrapKey().orElseThrow(), (resourceKey) -> modifyNoiseHolder(noiseHolder, noiseParameters));
     }
 
     abstract DensityFunction.NoiseHolder modifyNoiseHolder(DensityFunction.NoiseHolder noiseHolder, NormalNoise.NoiseParameters noiseParameters);
