27b3cf914dd1d60e07a67a5ff529a94a868b786e
Farcr <slender.farris@gmail.com>
2025-08-06 16:44:55 +0200

    downfall isle attempts failed, stuff's breakng

--- src/main/java/com/farcr/nomansland/common/world/generation/NMLBiomePlacements.java
+++ src/main/java/com/farcr/nomansland/common/world/generation/NMLBiomePlacements.java
@@ -261,13 +261,6 @@ public class NMLBiomePlacements {
                     neighbor(Tags.Biomes.IS_JUNGLE)
             );
         }
-
-        if (NMLConfig.TROPICAL_BEACH.get()) {
-            BiomePlacement.replaceOverworld(
-                    Biomes.MUSHROOM_FIELDS,
-                    NMLBiomes.DOWNFALL_ISLE, 0.5
-            );
-        }
     }
 
     public static void transitionalBiome(ResourceKey<Biome> firstBiome, ResourceKey<Biome> secondBiome, ResourceKey<Biome> transitionalBiome) {
