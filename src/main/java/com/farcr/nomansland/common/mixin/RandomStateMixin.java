e3160ff9a9585a8500d71a702e2d19eed12a4999
birsy <evan.c.muncy@gmail.com>
2025-08-10 16:04:33 -0400

    finish watershed system setup

--- src/main/java/com/farcr/nomansland/common/mixin/RandomStateMixin.java
+++ src/main/java/com/farcr/nomansland/common/mixin/RandomStateMixin.java
@@ -1,11 +1,15 @@
 package com.farcr.nomansland.common.mixin;
 
 import com.farcr.nomansland.common.world.watershed.Watershed;
+import com.farcr.nomansland.common.world.watershed.WatershedDensityFunctionVisitor;
 import com.farcr.nomansland.common.world.watershed.WatershedMap;
 import net.minecraft.core.HolderGetter;
 import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
+import net.minecraft.world.level.levelgen.NoiseRouter;
 import net.minecraft.world.level.levelgen.RandomState;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,18 +17,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(RandomState.class)
 public abstract class RandomStateMixin implements RandomStateExtension {
-    @Unique
-    WatershedMap nml$watershedMap;
+    @Shadow @Final private NoiseRouter router;
+    @Unique WatershedMap nml$watershedMap;
 
     @Inject(method = "<init>", at = @At("TAIL"))
     private void nml$init(NoiseGeneratorSettings settings, HolderGetter noiseParametersGetter, long levelSeed, CallbackInfo ci) {
-        this.nml$watershedMap = new WatershedMap((RandomState)(Object)this, 256);
-
-
-    }
-
-    @Override
-    public Watershed nml$getWatershed(int blockX, int blockZ) {
-        return nml$watershedMap.getOrCreateWatershed(Math.floorDiv(blockX, Watershed.WATERSHED_SIZE), Math.floorDiv(blockZ, Watershed.WATERSHED_SIZE));
+        this.nml$watershedMap = new WatershedMap((RandomState) (Object) this, 256);
+        this.router.mapAll(new WatershedDensityFunctionVisitor(this.nml$watershedMap));
     }
 }
--- src/main/java/com/farcr/nomansland/common/world/watershed/WatershedDensityFunctionVisitor.java
+++ src/main/java/com/farcr/nomansland/common/world/watershed/WatershedDensityFunctionVisitor.java
@@ -1,10 +1,20 @@
 package com.farcr.nomansland.common.world.watershed;
 
+import com.farcr.nomansland.common.world.densityfunction.CaveRiverDensityFunction;
 import net.minecraft.world.level.levelgen.DensityFunction;
 
 public class WatershedDensityFunctionVisitor implements DensityFunction.Visitor {
+    final WatershedMap watershedMap;
+
+    public WatershedDensityFunctionVisitor(WatershedMap watershedMap) {
+        this.watershedMap = watershedMap;
+    }
+
     @Override
     public DensityFunction apply(DensityFunction densityFunction) {
+        if (densityFunction instanceof CaveRiverDensityFunction caveRiverDensityFunction) {
+            return new CaveRiverDensityFunction(caveRiverDensityFunction.riverXOffset(), caveRiverDensityFunction.riverZOffset(), this.watershedMap);
+        }
         return densityFunction;
     }
 }
