f1ff779dead3392276d38e016116714bedea8421
tazercopter <yaseenelgamal20094@gmail.com>
2025-09-25 20:13:00 +0300

    datagen lang with everycomp

--- src/main/java/com/farcr/nomansland/datagen/DataGenEvents.java
+++ src/main/java/com/farcr/nomansland/datagen/DataGenEvents.java
@@ -19,7 +19,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
-@SuppressWarnings("unused")
 @EventBusSubscriber(modid = NoMansLand.MODID)
 public class DataGenEvents {
 
