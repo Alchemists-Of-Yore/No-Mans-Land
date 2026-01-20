ef8f80d7ec2c2ac331b8c89da4556b13fe17c46c
Farcr <slender.farris@gmail.com>
2026-01-20 22:17:58 +0100

    small asset

--- src/main/java/com/farcr/nomansland/datagen/NMLLanguageProvider.java
+++ src/main/java/com/farcr/nomansland/datagen/NMLLanguageProvider.java
@@ -69,7 +69,9 @@ public class NMLLanguageProvider extends LanguageProvider {
         add("effect.nomansland.pacified", "Pacified");
         add("entity.nomansland.ink_cloud", "Ink Cloud");
         add("painting.nomansland.sun.title", "Sun");
+        add("painting.nomansland.sun.author", "Farcr");
         add("painting.nomansland.white_eyes.title", "White Eyes");
+        add("painting.nomansland.sun.author", "Probleyes");
         add("nomansland.subtitles.entity.player.drink_milk", "Player drinks milk");
         add("nomansland.subtitles.entity.generic.sticky_cauldron_slide", "Sliding in a sticky cauldron");
         add("nomansland.subtitles.item.generic.consumed", "Item consumed");
