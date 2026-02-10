193c79e486a606227f8f1336eb681d9fe8e7d22c
Farcr <slender.farris@gmail.com>
2026-02-10 22:05:58 +0100

    NVM. i have to learn how to rotate voxelshape

--- src/main/java/com/farcr/nomansland/common/block/PlatformStairsBlock.java
+++ src/main/java/com/farcr/nomansland/common/block/PlatformStairsBlock.java
@@ -27,9 +27,11 @@ public class PlatformStairsBlock extends Block implements SimpleWaterloggedBlock
     public static final DirectionProperty HORIZONTAL_FACING = BlockStateProperties.HORIZONTAL_FACING;
     public static final BooleanProperty UNSTABLE = BlockStateProperties.UNSTABLE;
 
-    protected static final VoxelShape BOTTOM_SHAPE = Block.box(0.0D, 4.0D, 0.0D, 16.0D, 8.0D, 8.0D);
-    protected static final VoxelShape TOP_SHAPE = Block.box(0.0D, 12.0D, 8.0D, 16.0D, 16.0D, 16.0D);
-    protected static final VoxelShape SHAPE = Shapes.or(TOP_SHAPE, BOTTOM_SHAPE);
+    protected static final VoxelShape BOTTOM_SHAPE_X = Block.box(0.0D, 4.0D, 0.0D, 16.0D, 8.0D, 8.0D);
+    protected static final VoxelShape TOP_SHAPE_X = Block.box(0.0D, 12.0D, 8.0D, 16.0D, 16.0D, 16.0D);
+    protected static final VoxelShape BOTTOM_SHAPE_Z = Block.box(8.0D, 4.0D, 0.0D, 16.0D, 8.0D, 16.0D);
+    protected static final VoxelShape TOP_SHAPE_Z = Block.box(0.0D, 12.0D, 0.0D, 8.0D, 16.0D, 16.0D);
+    protected static final VoxelShape SHAPE = Shapes.or(TOP_SHAPE_X, BOTTOM_SHAPE_X);
 
     public PlatformStairsBlock(Properties properties) {
         super(properties);
