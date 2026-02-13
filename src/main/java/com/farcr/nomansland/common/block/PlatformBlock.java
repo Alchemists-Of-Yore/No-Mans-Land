c8d08c2f8f0676c9d80f2b7b15597b47cab34ed6
Sheddmer <69223831+Sheddmer@users.noreply.github.com>
2026-02-12 17:38:22 -0800

    platform model fixes + optimizations
    teeth shattered functionality

--- src/main/java/com/farcr/nomansland/common/block/PlatformBlock.java
+++ src/main/java/com/farcr/nomansland/common/block/PlatformBlock.java
@@ -2,9 +2,14 @@ package com.farcr.nomansland.common.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SimpleWaterloggedBlock;
@@ -15,6 +20,8 @@ import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import net.neoforged.neoforge.common.ItemAbilities;
+import net.neoforged.neoforge.common.ItemAbility;
 
 public class PlatformBlock extends Block implements SimpleWaterloggedBlock {
 
@@ -22,29 +29,12 @@ public class PlatformBlock extends Block implements SimpleWaterloggedBlock {
     public static final BooleanProperty UP = BlockStateProperties.UP;
     public static final EnumProperty<Direction.Axis> HORIZONTAL_AXIS = BlockStateProperties.HORIZONTAL_AXIS;
     public static final BooleanProperty UNSTABLE = BlockStateProperties.UNSTABLE;
-
-    protected static final VoxelShape BOTTOM_SHAPE = Block.box(0.0D, 4.0D, 0.0D, 16.0D, 8.0D, 16.0D);
     protected static final VoxelShape TOP_SHAPE = Block.box(0.0D, 12.0D, 0.0D, 16.0D, 16.0D, 16.0D);
+    protected static final VoxelShape BOTTOM_SHAPE = Block.box(0.0D, 4.0D, 0.0D, 16.0D, 8.0D, 16.0D);
 
     public PlatformBlock(Properties properties) {
         super(properties);
-        registerDefaultState(stateDefinition.any().setValue(WATERLOGGED, false).setValue(UP, false));
-    }
-
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(UP,WATERLOGGED,HORIZONTAL_AXIS);
-    }
-
-    protected FluidState getFluidState(BlockState state) {
-        return (Boolean)state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
-    }
-
-    protected BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor level, BlockPos currentPos, BlockPos facingPos) {
-        if ((Boolean)state.getValue(WATERLOGGED)) {
-            level.scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
-        }
-
-        return super.updateShape(state, facing, facingState, level, currentPos, facingPos);
+        registerDefaultState(stateDefinition.any().setValue(WATERLOGGED, false).setValue(UP, false).setValue(UNSTABLE, false));
     }
 
     protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
@@ -54,12 +44,41 @@ public class PlatformBlock extends Block implements SimpleWaterloggedBlock {
         return BOTTOM_SHAPE;
     }
 
+    @Override
+    public BlockState getToolModifiedState(BlockState state, UseOnContext context, ItemAbility itemAbility, boolean simulate) {
+        if (itemAbility == ItemAbilities.AXE_STRIP && !state.getValue(UNSTABLE)) {
+            return state.setValue(UNSTABLE, true);
+        }
+        return null;
+    }
+
+    protected FluidState getFluidState(BlockState state) {
+        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+    }
+
+    protected BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor level, BlockPos currentPos, BlockPos facingPos) {
+        if (state.getValue(WATERLOGGED)) {
+            level.scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
+        }
+        return super.updateShape(state, facing, facingState, level, currentPos, facingPos);
+    }
+
     public BlockState getStateForPlacement(BlockPlaceContext context) {
         FluidState fluidstate = context.getLevel().getFluidState(context.getClickedPos());
         return defaultBlockState()
                 .setValue(UP, Mth.frac(context.getClickLocation().y)>0.5)
                 .setValue(HORIZONTAL_AXIS, context.getHorizontalDirection().getAxis())
                 .setValue(WATERLOGGED, fluidstate.getType() == Fluids.WATER);
+    }
+
+    public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
+        if (entity.onGround() && state.getValue(UNSTABLE)) {
+            level.destroyBlock(pos, false, entity);
+        }
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(UP, WATERLOGGED, HORIZONTAL_AXIS, UNSTABLE);
     }
 }
 
--- src/main/java/com/farcr/nomansland/common/block/PlatformStairsBlock.java
+++ src/main/java/com/farcr/nomansland/common/block/PlatformStairsBlock.java
@@ -2,55 +2,69 @@ package com.farcr.nomansland.common.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.block.state.properties.DirectionProperty;
-import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.block.state.properties.*;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
-import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import net.neoforged.neoforge.common.ItemAbilities;
+import net.neoforged.neoforge.common.ItemAbility;
 
 public class PlatformStairsBlock extends Block implements SimpleWaterloggedBlock {
 
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
-    public static final DirectionProperty HORIZONTAL_FACING = BlockStateProperties.HORIZONTAL_FACING;
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
     public static final BooleanProperty UNSTABLE = BlockStateProperties.UNSTABLE;
-    //TODO: ADD UNSTABLE BLOCKSTATE, BREAKS WHEN STEPPED ON, NO DROPS
-    protected static final VoxelShape BOTTOM_SHAPE_X = Block.box(0.0D, 4.0D, 0.0D, 16.0D, 8.0D, 8.0D);
-    protected static final VoxelShape TOP_SHAPE_X = Block.box(0.0D, 12.0D, 8.0D, 16.0D, 16.0D, 16.0D);
-    protected static final VoxelShape BOTTOM_SHAPE_Z = Block.box(8.0D, 4.0D, 0.0D, 16.0D, 8.0D, 16.0D);
-    protected static final VoxelShape TOP_SHAPE_Z = Block.box(0.0D, 12.0D, 0.0D, 8.0D, 16.0D, 16.0D);
-    //TODO: MAKE THIS ROTATE BASED ON DIRECTION
-    protected static final VoxelShape SHAPE = Shapes.or(TOP_SHAPE_X, BOTTOM_SHAPE_X);
+    protected static final VoxelShape SHAPE_NORTH = Shapes.or(box(0.0D, 12.0D, 0.0D, 16.0D, 16.0D, 8.0D),box(0.0D, 4.0D, 8.0D, 16.0D, 8.0D, 16.0D));
+    protected static final VoxelShape SHAPE_SOUTH = Shapes.or(box(0.0D, 4.0D, 0.0D, 16.0D, 8.0D, 8.0D),box(0.0D, 12.0D, 8.0D, 16.0D, 16.0D, 16.0D));
+    protected static final VoxelShape SHAPE_EAST = Shapes.or(box(0.0D, 4.0D, 0.0D, 8.0D, 8.0D, 16.0D),box(8.0D, 12.0D, 0.0D, 16.0D, 16.0D, 16.0D));
+    protected static final VoxelShape SHAPE_WEST = Shapes.or(box(0.0D, 12.0D, 0.0D, 8.0D, 16.0D, 16.0D),box(8.0D, 4.0D, 0.0D, 16.0D, 8.0D, 16.0D));
 
     public PlatformStairsBlock(Properties properties) {
         super(properties);
-        registerDefaultState(stateDefinition.any().setValue(WATERLOGGED, false));
+        registerDefaultState(stateDefinition.any().setValue(WATERLOGGED, false).setValue(UNSTABLE, false));
     }
-    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
-        return SHAPE;
+
+    @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {
+        switch (state.getValue(FACING)) {
+            case NORTH:
+            default:
+                return SHAPE_NORTH;
+            case SOUTH:
+                return SHAPE_SOUTH;
+            case EAST:
+                return SHAPE_EAST;
+            case WEST:
+                return SHAPE_WEST;
+        }
     }
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
-        builder.add(WATERLOGGED,HORIZONTAL_FACING);
+
+    @Override
+    public BlockState getToolModifiedState(BlockState state, UseOnContext context, ItemAbility itemAbility, boolean simulate) {
+        if (itemAbility == ItemAbilities.AXE_STRIP && !state.getValue(UNSTABLE)) {
+            return state.setValue(UNSTABLE, true);
+        }
+        return null;
     }
 
     protected FluidState getFluidState(BlockState state) {
-        return (Boolean)state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
     }
 
     protected BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor level, BlockPos currentPos, BlockPos facingPos) {
-        if ((Boolean)state.getValue(WATERLOGGED)) {
+        if (state.getValue(WATERLOGGED)) {
             level.scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(level));
         }
         return super.updateShape(state, facing, facingState, level, currentPos, facingPos);
@@ -59,8 +73,18 @@ public class PlatformStairsBlock extends Block implements SimpleWaterloggedBlock
     public BlockState getStateForPlacement(BlockPlaceContext context) {
         FluidState fluidstate = context.getLevel().getFluidState(context.getClickedPos());
         return defaultBlockState()
-                .setValue(HORIZONTAL_FACING, context.getHorizontalDirection())
+                .setValue(FACING, context.getHorizontalDirection())
                 .setValue(WATERLOGGED, fluidstate.getType() == Fluids.WATER);
+    }
+
+    public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
+        if (entity.onGround() && state.getValue(UNSTABLE)) {
+            level.destroyBlock(pos, false, entity);
+        }
+    }
+
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(WATERLOGGED, FACING, UNSTABLE);
     }
 }
 
