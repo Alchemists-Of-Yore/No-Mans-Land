2a55fe5b10948538616f6101f33fa94553db0951
BeeIsYou <beeisyouandbest@gmail.com>
2026-03-13 17:13:00 +0800

    gave him normal pills

--- src/main/java/com/farcr/nomansland/client/renderer/entity/InvertedBellRenderer.java
+++ src/main/java/com/farcr/nomansland/client/renderer/entity/InvertedBellRenderer.java
@@ -5,28 +5,25 @@ import com.farcr.nomansland.client.handler.InvertedBellClientHandler;
 import com.farcr.nomansland.common.block.InvertedBellBlock;
 import com.farcr.nomansland.common.blockentity.InvertedBellBlockEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.block.model.BakedQuad;
+import net.minecraft.client.renderer.Sheets;
+import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.client.resources.model.ModelResourceLocation;
-import net.minecraft.core.Direction;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.phys.AABB;
 import org.joml.Quaternionf;
-
-import java.util.List;
 
 public class InvertedBellRenderer<T extends InvertedBellBlockEntity> implements BlockEntityRenderer<T> {
     public static final ModelResourceLocation BELL_MODEL = ModelResourceLocation.standalone(NoMansLand.location("block/inverted_bell_bell"));
     public static final ModelResourceLocation BEAM_MODEL = ModelResourceLocation.standalone(NoMansLand.location("block/inverted_bell_beam"));
 
+    private final BlockRenderDispatcher blockRenderer;
+
     public InvertedBellRenderer(BlockEntityRendererProvider.Context context) {
+        this.blockRenderer = context.getBlockRenderDispatcher();
     }
 
     @Override
@@ -44,34 +41,18 @@ public class InvertedBellRenderer<T extends InvertedBellBlockEntity> implements 
             if (rotation != null) {
                 poseStack.rotateAround(rotation, 0.5f, 2f - 4/16f, 0.5f);
             }
-            BakedModel model = Minecraft.getInstance().getModelManager().getModel(BELL_MODEL);
-            renderModelLists(model, packedLight, packedOverlay, poseStack, multiBufferSource.getBuffer(RenderType.cutout()));
+            BakedModel model = this.blockRenderer.getBlockModelShaper().getModelManager().getModel(BELL_MODEL);
+            this.blockRenderer.getModelRenderer().renderModel(poseStack.last(), multiBufferSource.getBuffer(Sheets.solidBlockSheet()), bell.getBlockState(), model,
+                    1, 1, 1, packedLight, packedOverlay);
             poseStack.popPose();
 
             poseStack.pushPose();
             poseStack.translate(0, 1, 0);
-            model = Minecraft.getInstance().getModelManager().getModel(BEAM_MODEL);
-            renderModelLists(model, packedLight, packedOverlay, poseStack, multiBufferSource.getBuffer(RenderType.cutout()));
+            model = this.blockRenderer.getBlockModelShaper().getModelManager().getModel(BEAM_MODEL);
+            this.blockRenderer.getModelRenderer().renderModel(poseStack.last(), multiBufferSource.getBuffer(Sheets.solidBlockSheet()), bell.getBlockState(), model,
+                    1, 1, 1, packedLight, packedOverlay);
             poseStack.popPose();
             poseStack.popPose();
-        }
-    }
-
-    private static void renderModelLists(BakedModel bakedModel, int packedLight, int packedOverlay, PoseStack poseStack, VertexConsumer vertexConsumer) {
-        RandomSource random = RandomSource.create();
-        for (Direction direction : Direction.values()) {
-            random.setSeed(42L);
-            renderQuadList(poseStack, vertexConsumer, bakedModel.getQuads(null, direction, random), packedLight, packedOverlay);
-        }
-
-        random.setSeed(42L);
-        renderQuadList(poseStack, vertexConsumer, bakedModel.getQuads(null, null, random), packedLight, packedOverlay);
-    }
-
-    private static void renderQuadList(PoseStack matrixStack, VertexConsumer vertexConsumer, List<BakedQuad> quads, int packedLight, int packedOverlay) {
-        PoseStack.Pose pose = matrixStack.last();
-        for (BakedQuad bakedQuad : quads) {
-            vertexConsumer.putBulkData(pose, bakedQuad, 1.0F, 1.0F, 1.0F, 1.0F, packedLight, packedOverlay);
         }
     }
 
