ef722e6f870b5b9666510f79050895bd1d1fb7a1
Farcr <slender.farris@gmail.com>
2026-03-26 04:51:17 +0100

    more maps

--- src/main/java/com/farcr/nomansland/datagen/NMLLanguageProvider.java
+++ src/main/java/com/farcr/nomansland/datagen/NMLLanguageProvider.java
@@ -164,7 +164,8 @@ public class NMLLanguageProvider extends LanguageProvider {
         add("nomansland.advancements.meet_friend_moon.description", "Meet the Friend Moon");
         add("nomansland.advancements.buddy_ascension.title", "Friend of my Friend");
         add("nomansland.advancements.buddy_ascension.description", "Find some company for the Friend Moon");
-        add("nomansland.filled_map.bell_sanctuary", "Ancient Sanctuary Map");
+        add("nomansland.filled_map.alchemist_ruins", "Ancestral Ruins Map");
+        add("nomansland.filled_map.bell_sanctuary", "Ancestral Sanctuary Map");
         add("nomansland.filled_map.mineshaft", "Lost Miner's Map");
         add("block.nomansland.resin_cauldron", "Cauldron Filled with Resin");
         add("block.nomansland.honey_cauldron", "Cauldron Filled with Honey");
