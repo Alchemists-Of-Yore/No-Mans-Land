b4b23bad2c54153b1e8c357eb12e4b7a9f26c6b4
Farcr <slender.farris@gmail.com>
2026-04-08 19:10:54 +0200

    somethings wrong

--- src/main/java/com/farcr/nomansland/client/particle/EntropyDustParticle.java
+++ src/main/java/com/farcr/nomansland/client/particle/EntropyDustParticle.java
@@ -11,7 +11,7 @@ public class EntropyDustParticle extends TextureSheetParticle {
     public EntropyDustParticle(ClientLevel level, double x, double y, double z, SpriteSet spriteSet) {
         super(level, x, y, z);
         this.setSprite(spriteSet.get(this.random.nextInt(4), 4));
-        this.gravity = 0.01F;
+        this.gravity = 0.00001F;
         this.lifetime = (int) (64.0 / (Math.random() * 0.8 + 0.2));
         this.roll = (float) Math.random() * ((float) Math.PI * 2F);
         this.oRoll = this.roll;
