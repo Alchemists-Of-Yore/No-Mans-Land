908982ad97b9dbb5ba01849887744f8ee9ba86d9
birsy <evan.c.muncy@gmail.com>
2026-04-16 14:08:44 -0400

    fix not compiling

--- src/main/java/com/farcr/nomansland/common/registry/entities/NMLSensors.java
+++ src/main/java/com/farcr/nomansland/common/registry/entities/NMLSensors.java
@@ -1,7 +1,6 @@
 package com.farcr.nomansland.common.registry.entities;
 
 import com.farcr.nomansland.NoMansLand;
-import com.farcr.nomansland.common.entity.cervidae.moose.deprecated.MooseThreatsSensor;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.ai.sensing.Sensor;
 import net.minecraft.world.entity.ai.sensing.SensorType;
@@ -11,8 +10,6 @@ import java.util.function.Supplier;
 
 public class NMLSensors {
     public static final DeferredRegister<SensorType<?>> SENSORS = DeferredRegister.create(Registries.SENSOR_TYPE, NoMansLand.MODID);
-
-    public static final Supplier<SensorType<MooseThreatsSensor>> MOOSE_THREATS = register("moose_threats", MooseThreatsSensor::new);
 
     private static <U extends Sensor<?>> Supplier<SensorType<U>> register(String key, Supplier<U> sensorSupplier) {
         return SENSORS.register(key, () -> new SensorType<>(sensorSupplier));
