906fa971f991e1a00c4f9fd2a7ef01d3157cc79e
SammySemicolon <69329424+SammySemicolon@users.noreply.github.com>
2026-04-25 18:26:25 +0200

    Tweak thatch sounds

--- src/main/java/com/farcr/nomansland/common/registry/blocks/NMLBlocks.java
+++ src/main/java/com/farcr/nomansland/common/registry/blocks/NMLBlocks.java
@@ -330,7 +330,7 @@ public class NMLBlocks {
             () -> new SlabBlock(ofFullCopy(NMLBlocks.EARTHEN_TILES.get())), BlockProperties.stoneLikeSlab());
 
     public static final BlockDefinition<Block> DROSS_TILES = register("dross_tiles",
-            () -> new Block(ofFullCopy(Blocks.PACKED_MUD).sound(NMLSounds.DROSS_TILES)), BlockProperties.stoneLike());
+            () -> new Block(ofFullCopy(Blocks.PACKED_MUD)), BlockProperties.stoneLike());
     public static final BlockDefinition<StairBlock> DROSS_TILE_STAIRS = register("dross_tile_stairs",
             () -> new StairBlock(DROSS_TILES.get().defaultBlockState(), ofFullCopy(NMLBlocks.DROSS_TILES.get())), BlockProperties.stoneLikeStairs());
     public static final BlockDefinition<SlabBlock> DROSS_TILE_SLAB = register("dross_tile_slab",
