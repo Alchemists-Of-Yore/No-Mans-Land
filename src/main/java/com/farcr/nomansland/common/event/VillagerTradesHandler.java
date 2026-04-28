267d2037b3f909e29d3ee61a5eb4cd287178dfd5
Farcr <slender.farris@gmail.com>
2026-04-29 00:51:40 +0200

    tweak map trades

--- src/main/java/com/farcr/nomansland/common/event/VillagerTradesHandler.java
+++ src/main/java/com/farcr/nomansland/common/event/VillagerTradesHandler.java
@@ -13,19 +13,19 @@ import net.neoforged.neoforge.event.village.WandererTradesEvent;
 @EventBusSubscriber(modid = NoMansLand.MODID)
 public class VillagerTradesHandler {
 
-    @SubscribeEvent
-    public static void onVillagerTrades(VillagerTradesEvent event) {
-        if (event.getType() == VillagerProfession.CARTOGRAPHER) {
-            event.getTrades().get(5).add(new VillagerTrades.TreasureMapForEmeralds(
-                    20, NMLTags.BELL_SANCTUARY, "nomansland.filled_map.bell_sanctuary",
-                    NMLMapDecorationTypes.BELL_SANCTUARY, 1, 30
-            ));
-            event.getTrades().get(5).add(new VillagerTrades.TreasureMapForEmeralds(
-                    20, NMLTags.ALCHEMIST_RUINS, "nomansland.filled_map.alchemist_ruins",
-                    NMLMapDecorationTypes.ALCHEMIST_RUINS, 1, 30
-            ));
-        }
-    }
+//    @SubscribeEvent
+//    public static void onVillagerTrades(VillagerTradesEvent event) {
+//        if (event.getType() == VillagerProfession.CARTOGRAPHER) {
+//            event.getTrades().get(5).add(new VillagerTrades.TreasureMapForEmeralds(
+//                    20, NMLTags.BELL_SANCTUARY, "nomansland.filled_map.bell_sanctuary",
+//                    NMLMapDecorationTypes.BELL_SANCTUARY, 1, 30
+//            ));
+//            event.getTrades().get(5).add(new VillagerTrades.TreasureMapForEmeralds(
+//                    20, NMLTags.ALCHEMIST_RUINS, "nomansland.filled_map.alchemist_ruins",
+//                    NMLMapDecorationTypes.ALCHEMIST_RUINS, 1, 30
+//            ));
+//        }
+//    }
 
     @SubscribeEvent
     public static void onWandererTrades(WandererTradesEvent event) {
@@ -34,7 +34,7 @@ public class VillagerTradesHandler {
                 NMLMapDecorationTypes.BELL_SANCTUARY, 1, 30
         ));
         event.getRareTrades().add(new VillagerTrades.TreasureMapForEmeralds(
-                20, NMLTags.ALCHEMIST_RUINS, "nomansland.filled_map.alchemist_ruins",
+                25, NMLTags.ALCHEMIST_RUINS, "nomansland.filled_map.alchemist_ruins",
                 NMLMapDecorationTypes.ALCHEMIST_RUINS, 1, 30
         ));
     }
