5ab9a72a554436c36759e929363f555a8f351258
DavigJ <david.j.jung@gmail.com>
2026-05-25 15:48:10 +0400

    broadened ward_repelled entity type to Enemy interface and added ward_repelled_blacklist, fixes #158

--- src/main/java/com/farcr/nomansland/common/event/MiscellaneousEvents.java
+++ src/main/java/com/farcr/nomansland/common/event/MiscellaneousEvents.java
@@ -37,7 +37,6 @@ import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.features.TreeFeatures;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.protocol.game.ClientboundRemoveMobEffectPacket;
 import net.minecraft.network.protocol.game.ClientboundUpdateMobEffectPacket;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
@@ -61,7 +60,6 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.monster.EnderMan;
 import net.minecraft.world.entity.monster.Enemy;
-import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.BlockItem;
@@ -401,7 +399,7 @@ public class MiscellaneousEvents {
     public static void onFinalizeMobSpawn(FinalizeSpawnEvent event) {
         if (event.getLevel() instanceof ServerLevel serverLevel
                 && event.getSpawnType() == MobSpawnType.NATURAL
-                && event.getEntity() instanceof Monster) {
+                && event.getEntity() instanceof Enemy && !event.getEntity().getType().is(NMLTags.WARD_REPELLED_BLACKLIST)) {
             WardedSpacesData wardedSpacesData = WardedSpacesData.get(serverLevel);
 
             event.setSpawnCancelled(wardedSpacesData.isWarded(serverLevel, event.getEntity().blockPosition()));
