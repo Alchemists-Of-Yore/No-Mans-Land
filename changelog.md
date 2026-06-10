13f3c1dc48cab59e1909bc0d89c9fa8385de96ae
tazer <tazercopter@gmail.com>
2026-06-11 02:44:22 +0300

    fix #218, incompat with larion

--- changelog.md
+++ changelog.md
@@ -1,3 +1,4 @@
 - Extended the incendiary arrow config option to disable ember spawning
 - Fixed mangrove trees placing dirt above their roots
 - Fixed monster anchors duplicating slime and magma cubes
+- Fixed mod incompatibility with Larion
--- src/main/java/com/farcr/nomansland/common/mixin/SteepMaterialConditionMixin.java
+++ src/main/java/com/farcr/nomansland/common/mixin/SteepMaterialConditionMixin.java
@@ -1,11 +1,10 @@
 package com.farcr.nomansland.common.mixin;
 
 import com.farcr.nomansland.common.world.surfacerule.NMLSteepMaterialCondition;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.world.level.levelgen.SurfaceRules;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SurfaceRules.Context.SteepMaterialCondition.class)
 public abstract class SteepMaterialConditionMixin extends SurfaceRules.LazyXZCondition {
@@ -14,20 +13,8 @@ public abstract class SteepMaterialConditionMixin extends SurfaceRules.LazyXZCon
         super(p_189622_);
     }
 
-    @Inject(method = "compute", at = @At(value = "HEAD"), cancellable = true)
-    private void fixMountainBug(CallbackInfoReturnable<Boolean> cir)  {
-        cir.setReturnValue(NMLSteepMaterialCondition.evaluate(context));
-
-//        int lastZ = Math.max(chunkZ - 1, 0), nextZ = Math.min(chunkZ + 1, 15);
-//        int lastZHeight = chunkaccess.getHeight(Heightmap.Types.WORLD_SURFACE_WG, chunkX, lastZ);
-//        int nextZHeight = chunkaccess.getHeight(Heightmap.Types.WORLD_SURFACE_WG, chunkX, nextZ);
-//        if (lastZHeight >= nextZHeight + 4) return true;
-//
-//        int lastX = Math.max(chunkX - 1, 0), nextX = Math.min(chunkX + 1, 15);
-//        int lastXHeight = chunkaccess.getHeight(Heightmap.Types.WORLD_SURFACE_WG, lastX, chunkZ);
-//        int nextXHeight = chunkaccess.getHeight(Heightmap.Types.WORLD_SURFACE_WG, nextX, chunkZ);
-//        if (nextXHeight >= lastXHeight + 4) return true;
-//
-//        return original;
+    @ModifyReturnValue(method = "compute", at = @At("RETURN"))
+    private boolean fixMountainBug(boolean original) {
+        return original || NMLSteepMaterialCondition.evaluate(context);
     }
 }
