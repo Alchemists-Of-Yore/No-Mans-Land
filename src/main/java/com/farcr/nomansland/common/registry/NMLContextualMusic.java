2d33ad1ce8bddca8fdbe1648b625d8bf290ab77f
tazer <tazercopter@gmail.com>
2026-06-23 20:13:56 +0300

    fix #245, anachronistic overriding other music

--- src/main/java/com/farcr/nomansland/common/registry/NMLContextualMusic.java
+++ src/main/java/com/farcr/nomansland/common/registry/NMLContextualMusic.java
@@ -1,7 +1,6 @@
 package com.farcr.nomansland.common.registry;
 
 import com.farcr.nomansland.NoMansLand;
-import com.farcr.nomansland.client.music.condition.AnachronisticMusicCondition;
 import com.farcr.nomansland.client.music.condition.GuidanceMusicCondition;
 import com.farcr.nomansland.client.music.condition.MusicCondition;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -12,6 +11,4 @@ public class NMLContextualMusic {
     public static final DeferredRegister<MusicCondition> CONTEXTUAL_MUSIC_REGISTRY = DeferredRegister.create(NMLRegistries.CONTEXTUAL_MUSIC, NoMansLand.MODID);
 
     public static final Supplier<GuidanceMusicCondition> GUIDANCE_CONTEXTUAL_MUSIC = CONTEXTUAL_MUSIC_REGISTRY.register("friend_moon_music", GuidanceMusicCondition::new);
-
-    public static final Supplier<AnachronisticMusicCondition> ANACHRONISTIC_CONTEXTUAL_MUSIC = CONTEXTUAL_MUSIC_REGISTRY.register("anachronistic_music", AnachronisticMusicCondition::new);
 }
